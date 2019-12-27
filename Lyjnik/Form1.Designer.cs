b38ce48c894fc27f1b857ad9f625615a3e5d4065
AlexeyninavI <50325364+AlexeyninavI@users.noreply.github.com>
2019-12-27 15:31:39 +0700

    Created

---
+++ Lyjnik/Form1.Designer.cs
@@ -0,0 +1,235 @@
+﻿namespace Lyjnik
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(707, 251);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Расчет";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.SystemColors.Control;
+            this.pictureBox3.Location = new System.Drawing.Point(12, 24);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(500, 250);
+            this.pictureBox3.TabIndex = 5;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox3_Paint);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(640, 27);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Масса";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(534, 24);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 8;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(531, 179);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 13);
+            this.label3.TabIndex = 12;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(531, 203);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 13);
+            this.label4.TabIndex = 13;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(534, 50);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 14;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(640, 53);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(101, 13);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "Начальная точка x";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(534, 76);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 16;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(534, 102);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 17;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(640, 105);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(77, 13);
+            this.label6.TabIndex = 18;
+            this.label6.Text = "Угол наклона";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.pictureBox2.Location = new System.Drawing.Point(275, 226);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(20, 20);
+            this.pictureBox2.TabIndex = 20;
+            this.pictureBox2.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(640, 79);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 13);
+            this.label1.TabIndex = 21;
+            this.label1.Text = "Время спуска";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(534, 128);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 20);
+            this.textBox5.TabIndex = 22;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(640, 131);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(150, 13);
+            this.label7.TabIndex = 23;
+            this.label7.Text = "Начальная скорость спуска";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(531, 226);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(0, 13);
+            this.label8.TabIndex = 24;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.ClientSize = new System.Drawing.Size(794, 293);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+    }
+}
+
---
+++ Lyjnik/Form1.cs
@@ -0,0 +1,261 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Drawing2D;
+using System.Timers;
+
+namespace Lyjnik
+{
+    public partial class Form1 : Form
+    {
+ 
+        private System.Timers.Timer animTimer;
+        
+        int x;
+        int y;
+
+        int angleForCalc; // угол наклона
+        double startSpeed; //начальная скорость
+        double startLocation; // стартовая позиций
+        int time; //время
+
+        // свойства горы
+        double heightMountain; 
+        double weightMountain;
+
+        //накопитель пикселей
+        double storageX = 0;
+        double storageY = 0;
+
+        //свойства горы для рисования
+        double lengthX;
+        double lengthY;
+    
+   
+        public void GetDrawParameters() // получить параметры для рисования
+        {
+            if (angleForCalc <= 45)
+            {
+                int b = (int)(Math.Tan(angleForCalc * (Math.PI / 180)) * 250);
+                heightMountain = 250 - b-10;
+                weightMountain = 0;
+                lengthX = 250;
+                lengthY = b;
+            }
+
+            else if
+                (angleForCalc > 45)
+            {
+                double b = (250 / Math.Tan(angleForCalc * (Math.PI / 180)));
+                heightMountain = 0;
+                lengthX = (int)b+15;
+                lengthY = 250;
+                weightMountain = 0;
+            }
+           
+        }
+
+        public void DrawMountain(PaintEventArgs e)
+
+        {
+            if(angleForCalc <= 45)
+            {
+                
+                int b = (int)(Math.Tan(angleForCalc * (Math.PI / 180)) * 250);
+                heightMountain =  250-b;
+                weightMountain = 0;
+                
+                e.Graphics.DrawLine(new Pen(Brushes.Black), 0, 250 - b, 250, 250);
+                DrawPlato(e, 250);
+            }
+
+            else if 
+                (angleForCalc > 45) {
+                
+
+                double b = (250/ Math.Tan(angleForCalc * (Math.PI / 180)));
+                heightMountain = 0;
+                weightMountain = 0;
+                
+                e.Graphics.DrawLine(new Pen(Brushes.Black), 0, 0, (int)b , 250);
+                DrawPlato(e, b);
+            }
+           
+            
+        }
+       
+      
+        public void DrawPlato(PaintEventArgs e, double b)
+
+        {
+           e.Graphics.DrawLine(new Pen(Brushes.Black), 550, pictureBox3.Height-1, (int)b , pictureBox3.Height-1);
+        }
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            pictureBox2.Parent = pictureBox3;
+          
+
+
+        }
+        private void SetAnimTimer()
+        {
+            animTimer = new System.Timers.Timer(20);
+            animTimer.Elapsed += OnTimedEventAnim;
+            animTimer.AutoReset = true;
+            animTimer.Enabled = true;
+            x = (int)weightMountain;
+            y = (int)heightMountain;
+        }
+       
+        private void OnTimedEventAnim(Object source, ElapsedEventArgs e) // Moving time
+        {
+
+
+
+            double changeX = lengthX  /lengthY;
+            double changeY = lengthY / lengthX;
+            if(angleForCalc < 15)
+            {
+                changeY *= 1.5;
+                changeX /= 2;
+
+
+            }
+            if (angleForCalc >= 15 && angleForCalc <20)
+            {
+                
+                changeX /= 2;
+
+
+            }
+            if (angleForCalc < 27 && angleForCalc >=20)
+            {
+                changeY *= 1.8;
+               
+
+
+            }
+            if (angleForCalc > 70)
+            {
+                changeX *= 2;
+
+            }
+            if (changeY > 1)
+            {
+                storageY = storageY + changeY;
+                double ostatok = Math.Round(changeY% 1,3);
+                double iY = storageY - ostatok;
+               storageY =  storageY - iY;
+                y+=(int)iY;
+            }
+           
+            else if (changeY <= 1)
+            {
+                storageY += changeY;
+                if(storageY >= 1)
+                {
+                    y++;
+                    storageY = storageY % 1;
+                }
+            }
+
+            if (changeX > 1)
+            {
+                storageX = storageX + changeX;
+                double ostatok = Math.Round(changeX % 1, 3);
+                double iX = storageX - ostatok;
+                storageX = storageX - iX;
+                x += (int)iX;
+            }
+
+            else if (changeX <= 1)
+            {
+                storageX += changeX;
+                if (storageX >= 1)
+                {
+                    x++;
+                    storageX = storageX % 1;
+                }
+            }
+            if (y >= 230)
+            {
+                animTimer.Enabled = false;
+                button1.Enabled = true;
+                x = y = 0;
+                return;
+
+            }
+
+            pictureBox2.Location = new Point(x, y);
+
+
+        }
+        public void SetPicBox()
+        {
+            pictureBox2.Location = new Point((int)weightMountain, (int)heightMountain);
+        }
+      
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double massa = double.Parse(textBox2.Text.ToString());
+            angleForCalc = int.Parse(textBox4.Text.ToString());
+            startLocation = double.Parse(textBox1.Text.ToString());
+            startSpeed = double.Parse(textBox5.Text.ToString());
+            time = int.Parse(textBox3.Text.ToString());
+
+            if (angleForCalc < 10 || angleForCalc > 80)
+            {
+                MessageBox.Show("Недопустимый угол");
+                return;
+            }
+
+            pictureBox3.Invalidate();
+            pictureBox3.Update();
+            GetDrawParameters();
+            SetPicBox();
+
+            label3.Text ="Ускорение лыжника: " + calculateAcceleration();
+            label4.Text = "Путь: " + calculateDistance(calculateSpeed(calculateAcceleration()),calculateAcceleration());
+            label8.Text = "Скорость: " + calculateSpeed(calculateAcceleration());
+            button1.Enabled = false;
+            SetAnimTimer();
+
+        }
+
+        private double calculateAcceleration()
+        {
+            double acceleration = Math.Round(9.8 * (Math.Sin(angleForCalc * (Math.PI / 180)))  - 0.45 * Math.Cos(angleForCalc * (Math.PI / 180)),4);
+            return acceleration;
+        }
+        private double calculateDistance(double speed,double acceleration)
+        {
+            double distance = startSpeed +speed*time+acceleration*time;
+            return distance;
+        }
+        private double calculateSpeed(double acceleration)
+        {
+            double speed = startSpeed+acceleration*time;
+            return speed ;
+        }
+
+
+      
+        private Font fnt = new Font("Arial", 10);
+        private void pictureBox3_Paint(object sender, PaintEventArgs e)
+        {
+            DrawMountain(e);
+        }
+
+      
+    }
+}
+
